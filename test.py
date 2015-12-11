f6709f6e69463c75c2d2d8a959e7ffd6052e5dd2
Taryn Shuler <tarynashleyshuler@gmail.com>
2015-12-10 21:46:06 -0800

    more testing

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import csv
 
 with open('christmas_songs.csv') as csvfile:
-	christmas_test = csv.reader(csvfile, delimiter=" ",quotechar="|")
+	christmas_test = csv.reader(csvfile,quotechar="|")
 	for row in christmas_test:
 		print ",".join(row)
-		
